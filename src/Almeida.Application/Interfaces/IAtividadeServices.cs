3907f98d30d512c8403652c17a45306f02dc7ffe
Caio Nunes <caio.nunes@minutoseguros.com.br>
2021-04-26 23:00:45 -0300

    Adicionado salvar na tela de Atividades

---
+++ src/Almeida.Application/Interfaces/IAtividadeServices.cs
@@ -0,0 +1,12 @@
+﻿using Almeida.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Almeida.Application.Interfaces
+{
+    public interface IAtividadeServices
+    {
+        int SalvarAtividade(AtividadeViewModel atividadeViewModel);
+    }
+}
---
+++ src/Almeida.Domain/Interfaces/IAtividadeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Almeida.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Almeida.Domain.Interfaces
+{
+    public interface IAtividadeRepository
+    {
+        int SalvarAtividade(Atividade atividade);
+    }
+}
